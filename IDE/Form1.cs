90047395e6588d73037c41ed384702c597ab3239
Pascal Lasnier <py@lasnier.com>
2019-11-19 10:02:43 +0000

    Implementing file editing functionality

--- IDE/Form1.cs
+++ IDE/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Controls;
 
 namespace IDE
 {
@@ -32,12 +33,15 @@ namespace IDE
         private void CreateEditorTab(string title, string text = "")
         {
             var newTab = new TabPage(title);
-            var newRichTextBox = new RichTextBox();
+            var newRichTextBox = new System.Windows.Forms.RichTextBox();
+
             newRichTextBox.Dock = DockStyle.Fill;
             newRichTextBox.WordWrap = false;
             newRichTextBox.Font = new Font("Consolas", 9);
             newRichTextBox.Text = text;
             newRichTextBox.AcceptsTab = true;
+            newRichTextBox.TextChanged += FileChanged;
+
             newTab.Controls.Add(newRichTextBox);
             tabControl1.TabPages.Add(newTab);
             tabControl1.SelectTab(newTab);
@@ -46,6 +50,29 @@ namespace IDE
         private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
         {
 
+        }
+
+        private void FileChanged(object sender, EventArgs e)
+        {
+            //if(e.GetType() == typeof(TextChangedEventArgs))
+            //{
+            //    var tce = (TextChangedEventArgs) e;
+                
+            //}
+
+        }
+
+        private class FileEdit
+        {
+            readonly TabPage Tab;
+            readonly System.Windows.Forms.RichTextBox TextBox;
+            public bool Saved { get; }
+            public string File { get; }
+
+            public FileEdit(TabPage tab)
+            {
+                Tab = tab
+            }
         }
     }
 }
