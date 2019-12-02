fd44e0d5609ee8cad1870a2b7f111b77922b75c8
Pascal Lasnier <py@lasnier.com>
2019-12-02 01:04:10 +0000

    Made a glorified ulong

--- Parse/Translator.cs
+++ Parse/Translator.cs
@@ -6,13 +6,34 @@ using System.Threading.Tasks;
 
 namespace Parse
 {
-    public class ByteCode
-    {
-        public 
-    }
-
     public static class Translator
     {
-        
+        public static SymbolicLong[,] Compile(string sourceCode)
+        {
+            ParserState state = ParserState.Global;
+
+            foreach (var character )
+
+            return;
+        }
+
+        private enum ParserState
+        {
+            Global,
+            Function
+        }
+
+        public struct SymbolicLong
+        {
+            public ulong CodeLong { get; private set; }
+
+            public SymbolicLong(ulong uulong)
+            {
+                CodeLong = uulong;
+            }
+
+            public static implicit operator ulong(SymbolicLong s) => s.CodeLong;
+            public static explicit operator SymbolicLong(ulong u) => new SymbolicLong(u);
+        }
     }
 }
